2ea0d51a68cd155d012c9b45bde695686c9366e3
Shadowrabbit <yingtu0401@gmail.com>
2025-10-29 13:48:59 +0800

    temp

---
+++ RabiConfigLib/ModBehaviour.cs
@@ -0,0 +1,12 @@
+﻿// ******************************************************************
+//       /\ /|       @file       ModBehaviour.cs
+//       \ V/        @brief      
+//       | "")       @author     Shadowrabbit, yingtu0401@gmail.com
+//       /  |                    
+//      /  \\        @Modified   2025-10-29 01:31:19
+//    *(__\_\        @Copyright  Copyright (c) 2025, Shadowrabbit
+// ******************************************************************
+
+namespace RabiConfigLib;
+
+public class ModBehaviour : Duckov.Modding.ModBehaviour;
